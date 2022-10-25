263d8a7824a20d342487ed1929bc813ea800f856
BarkinSungu <barkinsungu@gmail.com>
2022-10-25 19:04:57 +0300

    Project created. Min iOS version changed with 12.0

---
+++ ShoppingApp/ShoppingApp/Screens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingApp
+//
+//  Created by Barkın Süngü on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
