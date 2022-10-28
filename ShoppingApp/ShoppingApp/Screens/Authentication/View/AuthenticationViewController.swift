648e8bf414cce34e6aafe91899011ddd9b331688
BarkinSungu <barkinsungu@gmail.com>
2022-10-28 10:51:45 +0300

    Authentication added.

--- ShoppingApp/ShoppingApp/Screens/Authentication/View/AuthenticationViewController.swift
+++ ShoppingApp/ShoppingApp/Screens/Authentication/View/AuthenticationViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class AuthenticationViewController: UIViewController {
 
@@ -73,6 +74,75 @@ class AuthenticationViewController: UIViewController {
     }
 
     @IBAction func signButtonTapped(_ sender: UIButton) {
-        openMainPage()
+        if selectedSegment == "Sign In"{
+            login(emailText: emailTextField.text ?? "",
+                  passwordText: passwordTextField.text ?? "")
+        }
+        else if selectedSegment == "Sign Up"{
+            registation(emailText: emailTextField.text ?? "",
+                        passwordText: passwordTextField.text ?? "",
+                        secondPasswordText: secondPasswordTextField.text ?? "")
+        }
+    }
+    
+    func login(emailText: String, passwordText: String) {
+        print("login work")
+
+        if emailText != "" && passwordText != ""{
+            
+            Auth.auth().signIn(withEmail: emailText, password: passwordText) { authdataresult, error in
+                if error != nil {
+                    self.alertMessage(title: "Error", message: error?.localizedDescription ?? "Error")
+                    
+                }else{
+                    self.openMainPage()
+                }
+            }
+            
+        }else{
+            self.alertMessage(title: "Error", message: "You must enter email and password.")
+            
+        }
+    }
+    
+    func registation(emailText: String, passwordText: String, secondPasswordText: String){
+        print("registration work")
+        if emailText != "" && passwordText != "" && secondPasswordText != ""{
+            if passwordText == secondPasswordText{
+            
+                Auth.auth().createUser(withEmail: emailText, password: passwordText) { authdataresult, error in
+                    if error != nil{
+                        self.alertMessage(title: "Error", message: "Check internet connection.")
+                    }else{
+                        self.openMainPage()
+                        
+                    }
+                }
+            }else{
+                self.alertMessage(title: "Error", message: "Passwords must be same!")
+            }
+            
+        }else{
+            self.alertMessage(title: "Error", message: "You must enter email and password.")
+        }
+        
+    }
+    
+    func alertMessage(title: String, message: String) {
+       // Create the action buttons for the alert.
+       let defaultAction = UIAlertAction(title: "OK",
+                            style: .default) { (action) in
+        // Respond to user selection of the action.
+       }
+       
+       // Create and configure the alert controller.
+       let alert = UIAlertController(title: title,
+             message: message,
+             preferredStyle: .alert)
+       alert.addAction(defaultAction)
+            
+       self.present(alert, animated: true) {
+          // The alert was presented
+       }
     }
 }
